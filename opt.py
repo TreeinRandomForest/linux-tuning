e06ab497f034813ac87fd312c7fc8b15a2d76165
root <root@hp-z840-01.ml3.eng.bos.redhat.com>
2021-03-24 19:15:03 -0400

    hooking into dragonfly

--- opt.py
+++ opt.py
@@ -5,13 +5,23 @@ from dragonfly import load_config, maximize_function
 CONF_FILE_TEMPLATE = 'ml-perf-harness.conf.template'
 CONF_FILE = 'ml-perf.harness.conf'
 
-def objective(arr, domain=None):
+def objective(arr, domain=None, default_vals=None):
+    '''Objective function used by bayes opt
+    Responsible for:
+    1. Taking array input from dragonfly and transforming to dict
+    2. Use dict to write new conf file
+    3. Run ml-perf-harness to run workload and print metric to stdout
+    4. Parse stdout to get objective value
+    '''
+
     assert(len(arr)==len(domain))
 
-    kwargs = {a['name']:b for (a,b) in zip(domain, arr)}
-    #should also include fixed arguments
+    arr = [val[0] for val in arr] #since dragonfly wraps each val with dim=1 by default
 
-    write_config(outfile=CONF_FILE, templatefile=CONF_FILE_TEMPLATE, **kwargs)
+    params = {a['name']:b for (a,b) in zip(domain, arr)} #convert array to dict
+
+    write_config(outfile=CONF_FILE, templatefile=CONF_FILE_TEMPLATE, params=params, default_vals=default_vals)
+
     valid = check_config()
 
     if not valid:
@@ -76,10 +86,18 @@ def optimization_loop(capital=10):
 
     vals_default = read_vars('default.conf', vals=vals_limits)
 
-    domain = {}
+    domain = [{'name': 'MAX_SECTORS_KB', 'type': 'int', 'min': 1, 'max': 256, 'dim': 1},
+            ]
+    
+    def objective_temp(x):
+        return -(x[0]-100)**2
+
 
     config = load_config({'domain': domain})
-    val, point, history = maximize_function(objective, config.domain, capital)
+
+    objective_partial = lambda x: objective(x, domain=domain, default_vals=vals_default)
+    
+    val, point, history = maximize_function(objective_partial, config.domain, capital, config=config)
 
     return val, point, history
 
