231abd6b1782749318b8c003786614fe59fbe95c
root <root@kvm-01-guest04.lab.eng.rdu2.redhat.com>
2021-03-30 11:51:22 -0400

    adding scanner

--- opt.py
+++ opt.py
@@ -7,7 +7,7 @@ CONF_FILE = 'ml-perf.harness.conf'
 LOC = 'conf_files'
 it = 0
 
-def objective(arr, domain=None, default_vals=None):
+def objective(arr, domain=None, default_vals=None, return_std=False, n_iter=None):
     '''Objective function used by bayes opt
     Responsible for:
     1. Taking array input from dragonfly and transforming to dict
@@ -32,12 +32,19 @@ def objective(arr, domain=None, default_vals=None):
     if not valid:
         raise ValueError("Configuration is not valid")
 
-    o = subprocess.run(['bash', 'ml-perf-harness.sh', '-t', 'seq-disk-perf.sh'], capture_output=True)
+    if not n_iter:
+        o = subprocess.run(['bash', 'ml-perf-harness.sh', '-t', 'seq-disk-perf.sh'], capture_output=True)
+    else:
+        o = subprocess.run(['bash', 'ml-perf-harness.sh', '-t', 'seq-disk-perf.sh', '-n', str(n_iter)], capture_output=True)
+    
     o = [float(i) for i in o.stdout.decode('utf-8').split('\n') if len(i) > 0]
 
     shutil.copyfile(CONF_FILE, f'{LOC}/{CONF_FILE}.{it}')
+    
+    if return_std:
+        return np.mean(o), np.std(o)
 
-    return -np.mean(o)
+    return np.mean(o)
 
 def write_config(outfile='settings.conf', templatefile=None, default_vals = {}, params = {}):
     '''write passed config values to file
@@ -87,6 +94,27 @@ def read_vars(filename, vals={}):
                 continue
     return vals
 
+def scan_loop(n_iter=None):
+    vars_to_loop = [{'name': 'READ_AHEAD_KB', 'type': 'int', 'min': 0, 'max': 3, 'step_size': 1, 'dim': 1}]
+
+    vals_limits = read_vars('limits.sh')
+    vals_default = read_vars('default.conf', vals=vals_limits)
+
+    results = {}
+    for var in vars_to_loop:
+        domain = [var]
+
+        objective_partial = lambda x: objective(x, domain=domain, default_vals=vals_default, return_std=True, n_iter=n_iter)
+        
+        results[var['name']] = {}
+
+        for x in np.arange(var['min'], var['max'], var['step_size']):
+            metric_mean, metric_std = objective_partial([[x]])
+            results[var['name']][x] = (metric_mean, metric_std)
+            print(x, metric_mean, metric_std)
+
+    return results
+
 def optimization_loop(capital=10):
     vals_limits = read_vars('limits.sh')
     vals = read_vars('ml-perf-harness.conf', vals=vals_limits)
@@ -94,15 +122,15 @@ def optimization_loop(capital=10):
     vals_default = read_vars('default.conf', vals=vals_limits)
 
     domain = [
-                {'name': 'READ_LAT_NSEC', 'type': 'int', 'min': 0, 'max': 100000000, 'dim': 1},
-                {'name': 'WRITE_LAT_NSEC', 'type': 'int', 'min': 0, 'max': 100000000, 'dim': 1},
-                {'name': 'NR_REQUESTS', 'type': 'int', 'min': 4, 'max': 10000, 'dim': 1}, #queue depth
-                {'name': 'MAX_SECTORS_KB', 'type': 'int', 'min': 128, 'max': 1280, 'dim': 1}, #max IO size sent to device
+                #{'name': 'READ_LAT_NSEC', 'type': 'int', 'min': 0, 'max': 100000000, 'dim': 1},
+                #{'name': 'WRITE_LAT_NSEC', 'type': 'int', 'min': 0, 'max': 100000000, 'dim': 1},
+                #{'name': 'NR_REQUESTS', 'type': 'int', 'min': 4, 'max': 10000, 'dim': 1}, #queue depth
+                #{'name': 'MAX_SECTORS_KB', 'type': 'int', 'min': 128, 'max': 1280, 'dim': 1}, #max IO size sent to device
                 {'name': 'READ_AHEAD_KB', 'type': 'int', 'min': 0, 'max': 10000, 'dim': 1}, #amount of IO to read ahead into cache
-                {'name': 'WBT_LAT_USEC', 'type': 'int', 'min': 0, 'max': 10000, 'dim': 1}, #target latency for reads. throttle writes otherwise
-                {'name': 'DIRTY_RATIO', 'type': 'int', 'min': 0, 'max': 100, 'dim': 1},
-                {'name': 'DIRTY_BACKGROUND_RATIO', 'type': 'int', 'min': 0, 'max': 100, 'dim': 1},
-                {'name': 'SWAPPINESS', 'type': 'int', 'min': 0, 'max': 100, 'dim': 1},
+                #{'name': 'WBT_LAT_USEC', 'type': 'int', 'min': 0, 'max': 10000, 'dim': 1}, #target latency for reads. throttle writes otherwise
+                #{'name': 'DIRTY_RATIO', 'type': 'int', 'min': 0, 'max': 100, 'dim': 1},
+                #{'name': 'DIRTY_BACKGROUND_RATIO', 'type': 'int', 'min': 0, 'max': 100, 'dim': 1},
+                #{'name': 'SWAPPINESS', 'type': 'int', 'min': 0, 'max': 100, 'dim': 1},
             ]
     
     config = load_config({'domain': domain})
